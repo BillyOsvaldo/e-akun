8e713ce18293bdf943bcef4b1eb169231f574df7
dckristiono <dckristiono@gmail.com>
2017-12-27 22:29:25 +0700

    fix

--- src/services/checkuser/checkuser.class.js
+++ src/services/checkuser/checkuser.class.js
@@ -1,5 +1,3 @@
-import errors from '@feathersjs/errors';
-
 module.exports = class checkUser {
   async find(params) {
     const checkMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
