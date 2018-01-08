95c0afe5acd5b808ab244e79c83703e2c2ce8096
dckristiono <dckristiono@gmail.com>
2018-01-08 11:38:11 +0700

    test

--- src/services/userapp/userapp.class.js
+++ src/services/userapp/userapp.class.js
@@ -1,12 +1,13 @@
 module.exports = class userApp {
+  async get (params) {
+    console.log(params)
+  }
+
   async find (params) {
     const app = this.app.get('appid')
     const userid = params.query.userid
     let _output = []
     if (typeof app !== 'undefined' && typeof userid !== 'undefined') {
-      console.log('OK!')
-      console.log('userid', userid)
-      console.log('app', app)
       const _userperm = await this.app.service('userpermissions')
         .find({
           query: {
