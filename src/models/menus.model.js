d108ccc85495b7fb478b3667a568c090ca1030bd
dckristiono <dckristiono@gmail.com>
2018-01-10 10:49:28 +0700

    test

--- src/models/menus.model.js
+++ src/models/menus.model.js
@@ -1,12 +1,24 @@
 // menus-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const menus = new Schema({
-    text: { type: String, required: true }
+    name: { type: String, required: true },
+    icon: { type: String, required: true },
+    to: { type: String, required: true },
+    desc: { type: String, required: true },
+    color: { type: String, required: true },
+    roles: [{
+      type: mongooseClient.Schema.Types.ObjectId,
+      ref: 'roles'
+    }],
+    permissions: [{
+      type: mongooseClient.Schema.Types.ObjectId,
+      ref: 'permissions'
+    }]
   }, {
     timestamps: true
   });
