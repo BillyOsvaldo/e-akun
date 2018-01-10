3983cc988804d77a6118dfb294aa99f3ebe02a1b
dckristiono <dckristiono@gmail.com>
2018-01-10 10:54:49 +0700

    test

--- src/services/menuapp/menuapp.class.js
+++ src/services/menuapp/menuapp.class.js
@@ -2,7 +2,6 @@ module.exports = class menuApp {
   async find (params) {
     const permission = params.query.permission
     const role = params.query.role
-    let _output = []
     if (typeof permission !== 'undefined' && typeof role !== 'undefined') {
       console.log(permission)
       console.log(role)
@@ -16,14 +15,8 @@ module.exports = class menuApp {
           }
         })
 
-      console.log(_menu)
-
-      if (_menu.total === 1) {
-          _output = _menu.data[0]
-      }
-
     }
-    return _output
+    return _menu
   }
 
   setup (app) {
