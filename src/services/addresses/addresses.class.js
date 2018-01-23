459b751aa5db09620c772490f95bcf52435d55e9
dckristiono <dckristiono@gmail.com>
2018-01-23 14:01:34 +0700

    test

--- src/services/addresses/addresses.class.js
+++ src/services/addresses/addresses.class.js
@@ -3,10 +3,16 @@ module.exports = class addresses {
     const action = params.query.action
     let _output = []
     if (typeof action !== 'undefined') {
-      if (action === 'propinsi') {
+      if (action === 'provinsi') {
         const _prop = await this.app.service('postcodes')
           .Model.distinct('propinsi')
         return _prop
+      } else if (action === 'kotakab') {
+        if (typeof params.query.provinsi !== 'undefined') {
+          const _kotakab = await this.app.service('postcodes')
+            .Model.distinct('kotakab', {'propinsi': params.query.provinsi})
+          return _kotakab
+        }
       }
     }
   }
