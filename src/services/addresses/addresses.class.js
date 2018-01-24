dde21e492e6e6369008cc48456301c784ab14517
dckristiono <dckristiono@gmail.com>
2018-01-24 08:14:44 +0700

    change addresses

--- src/services/addresses/addresses.class.js
+++ src/services/addresses/addresses.class.js
@@ -1,32 +1,36 @@
 module.exports = class addresses {
   async find (params) {
-    const action = params.query.action
-    let _output = []
-    if (typeof action !== 'undefined') {
-      if (action === 'provinsi') {
-        const _prop = await this.app.service('postcodes')
-          .Model.distinct('propinsi')
-        return _prop
-      } else if (action === 'kotakab') {
-        if (typeof params.query.provinsi !== 'undefined') {
-          const _kotakab = await this.app.service('postcodes')
-            .Model.distinct('kotakab', {'propinsi': params.query.provinsi})
-          return _kotakab
-        }
-      } else if (action === 'kecamatan') {
-        if (typeof params.query.kotakab !== 'undefined') {
-          const _kecamatan = await this.app.service('postcodes')
-            .Model.distinct('kecamatan', {'kotakab': params.query.kotakab})
-          return _kecamatan
-        }
-      } else if (action === 'kelurahan') {
+    let _output = {
+      provinsi: [],
+      kotakab: [],
+      kecamatan: [],
+      kelurahan: [],
+      id: null,
+      postcode: null
+    }
+    const _prop = await this.app.service('postcodes')
+      .Model.distinct('propinsi')
+    _output.provinsi = _prop
+
+    if (typeof params.query.provinsi !== 'undefined') {
+      const _kotakab = await this.app.service('postcodes')
+        .Model.distinct('kotakab', {'propinsi': params.query.provinsi})
+      _output.kotakab = _kotakab
+
+      if (typeof params.query.kotakab !== 'undefined') {
+        const _kecamatan = await this.app.service('postcodes')
+          .Model.distinct('kecamatan', {'kotakab': params.query.kotakab})
+        _output.kecamatan = _kecamatan
+
         if (typeof params.query.kecamatan !== 'undefined') {
           const _kelurahan = await this.app.service('postcodes')
-            .Model.distinct(['kelurahan', 'kodepos'], {'kecamatan': params.query.kecamatan})
-          return _kelurahan
+            .Model.distinct('kelurahan', {'kecamatan': params.query.kecamatan})
+          _ouput.kelurahan = _kelurahan
         }
       }
     }
+
+    return _output
   }
 
   setup (app) {
