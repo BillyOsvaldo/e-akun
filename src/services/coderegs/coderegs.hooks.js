26826e711dcb09ea1e0e7c6dc30bf2bef66f7a6c
wildan <wilfathan@gmail.com>
2018-02-07 10:11:39 +0700

    Add hook on coderegs service: orderByCreatedAtDesc

--- src/services/coderegs/coderegs.hooks.js
+++ src/services/coderegs/coderegs.hooks.js
@@ -1,11 +1,12 @@
 const { authenticate } = require('feathers-authentication').hooks;
 const generateCode = require('../../hooks/generate_codereg')
 const sendEmailCodeReg = require('../../hooks/send_email_codereg')
+const orderByCreatedAtDesc = require('../../hooks/order_by_created_at_desc')
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [ orderByCreatedAtDesc ],
     get: [],
     create: [ generateCode, sendEmailCodeReg ],
     update: [],
