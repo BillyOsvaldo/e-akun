c4129136cdd1b7bcf18dc59d3db1db79ede32293
wildan <wilfathan@gmail.com>
2018-02-24 08:21:43 +0700

    Service usersmanagement patch: move fn from userapp patch 2

--- src/services/userapp/userapp.class.js
+++ src/services/userapp/userapp.class.js
@@ -208,33 +208,6 @@ module.exports = class userApp {
   }
 
   async patch (id, data, params) {
-    if (data.update === 'account') {
-      console.log('account')
-      let current = await this.app.service('users').get(id)
-      let compare = await bcrypt.compareSync(data.comparepassword, current.password)
-      if (!compare) {
-        throw new errors.BadRequest('Kata Sandi Salah.', {})
-      } else {
-        delete data.comparepassword
-        delete data.update
-        const _user = await this.app.service('users')
-          .patch(id, data, params)
-        return _user
-      }
-    } else if(data.update === 'manage_account') {
-      console.log('manage_account')
-
-      const _user = await this.app.service('users').patch(id, data, params)
-      return _user
-    } else if (data.update === 'profile') {
-      console.log('profile')
-      let profile_id = data.id
-      delete data.id
-      delete data.update
-      await this.app.service('profiles').patch(profile_id, data, params)
-      const _user = await this.app.service('users').get(id)
-      return _user
-    }
   }
 
   async remove(id, params) {
