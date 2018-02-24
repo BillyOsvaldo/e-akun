ce659b519e081f8726054c85c4b2d63c7be2947c
wildan <wilfathan@gmail.com>
2018-02-24 08:36:04 +0700

    Service usersmanagment: clone all fn and hooks from userapp

--- src/services/usersmanagement/usersmanagement.hooks.js
+++ src/services/usersmanagement/usersmanagement.hooks.js
@@ -1,21 +1,50 @@
+const { authenticate } = require('feathers-authentication').hooks;
+const commonHooks = require('feathers-hooks-common');
+const { restrictToOwner } = require('feathers-authentication-hooks');
+const { hashPassword } = require('feathers-authentication-local').hooks;
+const addTimestamp = require('../../hooks/add_timestamp')
+const userappHook = require('../../hooks/userapp_service')
+const permissions = require('../../hooks/permissions')
+
+const restrict = [
+  authenticate('jwt'),
+  restrictToOwner({
+    idField: '_id',
+    ownerField: '_id'
+  })
+]
+
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    find: [ authenticate('jwt') ],
+    //find: [ authenticate('jwt'), userappHook.populate ],
+    get: [ ...restrict ],
+    create: [ userappHook.checkPns ],
+    update: [ authenticate('jwt') ],
+    patch: [
+      authenticate('jwt'),
+      permissions.restrict
+    ],
+    remove: [ authenticate('jwt') ]
   },
 
   after: {
-    all: [],
-    find: [],
-    get: [],
+    all: [
+      addTimestamp,
+      commonHooks.when(
+        hook => hook.params.provider,
+        commonHooks.when(
+          hook => (typeof hook.result !== 'undefined' && typeof hook.result.password !== 'undefined'),
+          commonHooks.discard('password')
+        )
+      )
+    ],
+    find: [ userappHook.populate ],
+    get: [ userappHook.populate ],
     create: [],
     update: [],
-    patch: [],
+    patch: [ userappHook.populate ],
     remove: []
   },
 
