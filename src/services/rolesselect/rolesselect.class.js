c0eb48cd224a1878e17659e28105d8f34609e890
wildan <wilfathan@gmail.com>
2018-03-10 14:11:19 +0700

    Service permissionsselect & service rolesselect: by pass

--- src/services/rolesselect/rolesselect.class.js
+++ src/services/rolesselect/rolesselect.class.js
@@ -1,24 +1,6 @@
 module.exports = class addresses {
   async find (params) {
-    const qRegex = new RegExp(params.query.q, "i")
-
-    var query = {
-      $skip: params.query.$skip,
-      $limit: params.query.$limit,
-      $select: params.query.$select,
-      $sort: params.query.$sort
-    }
-
-    if(params.query.q !== undefined) {
-      query.$or = [
-        { _id: params.query.q },
-        { name: qRegex },
-        { tag: qRegex }
-      ]
-    }
-
-    const _result = await this.app.service('roles').find({ query: query })
-    return _result
+    return await this.app.service('roles').find(params)
   }
 
   setup (app) {
