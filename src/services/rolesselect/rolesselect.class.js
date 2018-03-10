38d6c4d1ba0af4076e0ac2e08c3b7ffb451537aa
wildan <wilfathan@gmail.com>
2018-03-10 11:24:16 +0700

    Service permissionsselect & rolesselect: fix bug

--- src/services/rolesselect/rolesselect.class.js
+++ src/services/rolesselect/rolesselect.class.js
@@ -1,20 +1,23 @@
 module.exports = class addresses {
   async find (params) {
-    const q = new RegExp(params.query.q, "i")
-    const _result = await this.app.service('roles')
-      .find({
-        query: {
-          $or: [
-            { _id: params.query.q },
-            { name: q },
-            { tag: q }
-          ],
-          $skip: params.query.$skip,
-          $limit: params.query.$limit,
-          $select: params.query.$select,
-          $sort: params.query.$sort
-        }
-      })
+    const qRegex = new RegExp(params.query.q, "i")
+
+    var query = {
+      $skip: params.query.$skip,
+      $limit: params.query.$limit,
+      $select: params.query.$select,
+      $sort: params.query.$sort
+    }
+
+    if(params.query.q !== undefined) {
+      query.$or = [
+        { _id: params.query.q },
+        { name: qRegex },
+        { tag: qRegex }
+      ]
+    }
+
+    const _result = await this.app.service('roles').find({ query: query })
     return _result
   }
 
