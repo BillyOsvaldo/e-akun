9da198bd998fbeb22b73ed1ea957ee147155d9fc
wildan <wilfathan@gmail.com>
2018-03-13 08:27:59 +0700

    Add service organizationstructures

---
+++ src/services/organizationstructures/organizationstructures.service.js
@@ -0,0 +1,29 @@
+// Initializes the `organizationstructures` service on path `/organizationstructures`
+const createService = require('feathers-mongoose');
+const createModel = require('../../models/organizationstructures.model');
+const hooks = require('./organizationstructures.hooks');
+const filters = require('./organizationstructures.filters');
+
+module.exports = function () {
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'organizationstructures',
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/organizationstructures', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('organizationstructures');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
