6b068ea4648c97841df7f753d8fe278d659ae585
wildan <wilfathan@gmail.com>
2018-03-22 09:28:55 +0700

    Service usersmanagement, usersselect: able to search and match by `profiles.name`

--- src/services/usersselect/usersselect.class.js
+++ src/services/usersselect/usersselect.class.js
@@ -1,12 +1,12 @@
 module.exports = class {
   async find (params) {
     params.paginate = false
-    const docs = await this.app.service('users').find(params)
+    const docs = await this.app.service('usersmanagement').find(params)
     return {
-      "total": docs.length,
-      "limit": docs.length,
+      "total": docs.data.length,
+      "limit": docs.data.length,
       "skip": 0,
-      "data": docs
+      "data": docs.data
     }
   }
 
