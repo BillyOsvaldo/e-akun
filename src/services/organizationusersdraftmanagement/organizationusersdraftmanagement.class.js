7c62b83874309e474931cee4a4ee62af548d9ad2
wildan <wilfathan@gmail.com>
2018-03-26 11:39:20 +0700

    Service organizationusersmanagement: Move autofill organizationstructuresusers to organizationusersdraftmanagement

--- src/services/organizationusersdraftmanagement/organizationusersdraftmanagement.class.js
+++ src/services/organizationusersdraftmanagement/organizationusersdraftmanagement.class.js
@@ -1,9 +1,21 @@
 module.exports = class {
   async create(data, params) {
+    // if context.data.organizationStructure is not empty then create organizationstructuresusers
+    if(data.organizationStructure && data.organizationStructureStartDate) {
+      const dataOrganizationstructuresusers = {
+        user: data.user,
+        organizationstructure: data.organizationStructure,
+        startDate: data.organizationStructureStartDate
+      }
+
+      await this.app.service('organizationstructuresusersdraft').create(dataOrganizationstructuresusers, params)
+    }
+
     return await this.app.service('organizationusersdraft').create(data, params)
   }
 
   async find(params) {
+    // all field organizationusersdraft
     return await this.app.service('organizationusersdraft').find(params)
   }
 
