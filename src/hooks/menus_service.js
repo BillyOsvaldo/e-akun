fad073db762dbfc11932feefcc24194c863a1ac3
wildan <wilfathan@gmail.com>
2018-03-28 12:28:31 +0700

    Service menus: disable pagination

--- src/hooks/menus_service.js
+++ src/hooks/menus_service.js
@@ -5,4 +5,17 @@ menusHook.generateOrder = async context => {
   context.data.order = (await Menus.count()) + 1
 }
 
+menusHook.paginationBefore = async context => {
+  context.params.paginate = false
+}
+
+menusHook.paginationAfter = async context => {
+  context.result = {
+    "total": context.result.length,
+    "limit": context.result.length,
+    "skip": 0,
+    "data": context.result
+  }
+}
+
 module.exports = menusHook
