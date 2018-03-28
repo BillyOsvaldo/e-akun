e4e82deb5a98d8b0c1ed75ea07c504bb192dfac6
dckristiono <dckristiono@gmail.com>
2018-03-28 09:24:06 +0700

    fix bug where check organizationusers

--- src/hooks/organizationusers_service.js
+++ src/hooks/organizationusers_service.js
@@ -156,18 +156,15 @@ organizationusersHook.publish = async (context) => {
       await organizationUsers.create(docOrganizationUsersDraft)
     endif
   */
-  console.log('docOrganizationUsersDraft', docOrganizationUsersDraft)
   
   const whereOrganizationUsers = {
-    user: docOrganizationUsersDraft.user,
-    organization: docOrganizationUsersDraft.organization,
-    endDate: null
+    query: {
+      user: docOrganizationUsersDraft.user,
+      organization: docOrganizationUsersDraft.organization,
+      endDate: null
+    }
   }
-  
-  console.log('where?', whereOrganizationUsers)
- 
   const docs = await organizationUsers.find(whereOrganizationUsers)
-  console.log('docs', docs)
   const isOrganizationUsersExist = Boolean(docs.total)
   if(!isOrganizationUsersExist) {
     await organizationUsersManagement.create(docOrganizationUsersDraft)
