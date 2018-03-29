1e6fe55ce6442fb48138715b148517f384e0cd82
wildan <wilfathan@gmail.com>
2018-03-29 13:16:22 +0700

    Service coderegsmanagement: Auto add row organizationusersdraft and add row organizationstructuresusersdraft

--- src/services/coderegsmanagement/coderegsmanagement.class.js
+++ src/services/coderegsmanagement/coderegsmanagement.class.js
@@ -1,6 +1,61 @@
+const errors = require('@feathersjs/errors')
+
+/*
+
+format:
+{
+  "email": "example@gmail.com" -> coderegs.email
+  "organization": "123123" -> organizationusersdraft.organization
+  "organizationUsersStartDate": "2018-03-29" -> organizationusersdraft.startDate
+  "inside": "12313" -> organizationusersdraft.inside
+  "organizationstructure": "123123" -> organizationstructuresusersdraft.organizationstructure
+  "organizationStructuresUsersStartDate": "2018-03-29" -> organizationstructuresusersdraft.startDate
+}
+
+*/
+
 module.exports = class CoderegsManagement {
   async create(data, params) {
-    return await this.app.service('coderegs').create(data, params)
+    const validate = () => {
+      const requiredFields = [ 'email', 'organization', 'organizationstructure', 'organizationUsersStartDate', 'organizationStructuresUsersStartDate' ]
+      for(let field of requiredFields) {
+        if(!data[field]) throw new errors.BadRequest('Field ' + field + ' must be exist')
+      }
+    }
+
+    const handleOrganizationUsersDraft = async (coderegId) => {
+      const dataInner = {
+        user: coderegId,
+        organization: data.organization,
+        inside: data.inside,
+        startDate: data.organizationUsersStartDate
+      }
+
+      await this.app.service('organizationusersdraftmanagement').create(dataInner)
+
+      delete data.organization
+      delete data.inside
+      delete data.organizationUsersStartDate
+    }
+
+    const handleOrganizationStructuresUsersDraft = async (coderegId) => {
+      const dataInner = {
+        user: coderegId,
+        organizationstructure: data.organizationstructure,
+        startDate: data.organizationStructuresUsersStartDate
+      }
+
+      await this.app.service('organizationstructuresusersdraftmanagement').create(dataInner)
+
+      delete data.organizationstructure
+      delete data.organizationStructuresUsersStartDate
+    }
+
+    validate()
+    const docCoderegs = await this.app.service('coderegs').create(data, params)
+    await handleOrganizationUsersDraft(docCoderegs._id)
+    await handleOrganizationStructuresUsersDraft(docCoderegs._id)
+    return docCoderegs
   }
 
   async find(params) {
