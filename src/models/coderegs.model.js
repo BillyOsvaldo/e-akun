9571e8230cd2449cd3e0f66ae62f236d15f36ce4
f4th4n <wilfathan@gmail.com>
2018-04-04 07:55:42 +0700

    Coderegs model: add field organization

--- src/models/coderegs.model.js
+++ src/models/coderegs.model.js
@@ -5,6 +5,10 @@ const coderegsModel = function(app) {
     email: { type: String, required: true },
     code: { type: String, required: true },
     status: { type: Boolean, required: true, default: false },
+    organization: {
+      type: mongooseClient.Schema.Types.ObjectId,
+      ref: 'organizations'
+    }
   }, {
     timestamps: true
   });
