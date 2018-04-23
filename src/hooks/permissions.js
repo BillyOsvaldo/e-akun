fa1c46dae1220b9a6c90f29e8c066659c0aa9c49
f4th4n <wilfathan@gmail.com>
2018-04-23 09:14:41 +0700

    Permissions.apiOrJwt handler fix

--- src/hooks/permissions.js
+++ src/hooks/permissions.js
@@ -101,7 +101,11 @@ permissions.restrict = (ownerField = '_id', idField = '_id') => {
 permissions.apiOrJWT = (context) => {
   commonHooks.iffElse(
     // if the specific header is included
-    ctx => ctx.params.headers['x-api-key'],
+    ctx => {
+      if(!ctx.params.headers) return false
+
+      ctx.params.headers['x-api-key']
+    },
     // authentication with this strategy
     authenticate('apiKey'),
     // else fallback on the jwt strategy
